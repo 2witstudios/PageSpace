ca2b79c4144945248efde91ecb323f02c395f0ef
DaisyDebate <2witstudios@gmail.com>
2025-10-20 21:36:55 -0500

    updated build for proper electron pre script

---
+++ apps/desktop/electron.vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'electron-vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  main: {
+    build: {
+      outDir: 'out/main',
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'src/main/index.ts'),
+        },
+        output: {
+          format: 'es',
+        },
+      },
+    },
+  },
+  preload: {
+    build: {
+      outDir: 'out/preload',
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'src/preload/index.ts'),
+        },
+        output: {
+          format: 'cjs',
+          entryFileNames: '[name].js',
+        },
+      },
+    },
+  },
+});
