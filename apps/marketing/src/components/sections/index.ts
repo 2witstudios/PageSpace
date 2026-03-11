e2319c03830404df83734a704aee9bb347c889ba
2Wits <2witstudios@gmail.com>
2026-03-10 19:38:48 -0500

    refactor(marketing): decompose landing pages, slash complexity (#753)
    * refactor(marketing): decompose monolithic landing pages into section components
    
    * refactor(marketing): decompose monolithic landing pages into section components

---
+++ apps/marketing/src/components/sections/index.ts
@@ -0,0 +1,8 @@
+export { HeroSection } from "./HeroSection";
+export { FeaturesPreviewSection } from "./FeaturesPreviewSection";
+export { PageTreeSection } from "./PageTreeSection";
+export { DocumentsSection } from "./DocumentsSection";
+export { ChannelsSection } from "./ChannelsSection";
+export { TasksSection } from "./TasksSection";
+export { CalendarSection } from "./CalendarSection";
+export { CTASection } from "./CTASection";
